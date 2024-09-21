2feb27572517b3764836f40d99d4efa9d2d9dfcd
pmqueiroz <pedroqueiroz593@gmail.com>
2024-09-21 13:50:34 -0300

    refactor: remove unused visitors

--- ast/expression.go
+++ ast/expression.go
@@ -4,31 +4,11 @@ import (
 	"github.com/pmqueiroz/umbra/tokens"
 )
 
-type ExpressionVisitor interface {
-	VisitAssignExpression(expr AssignExpression)
-	VisitBinaryExpression(expr Expression)
-	VisitCallExpression(expr CallExpression)
-	VisitGroupingExpression(expr GroupingExpression)
-	VisitLiteralExpression(expr LiteralExpression)
-	VisitHashmapExpression(expr HashmapExpression)
-	VisitArrayExpression(expr ArrayExpression)
-	VisitLogicalExpression(expr LogicalExpression)
-	VisitUnaryExpression(expr UnaryExpression)
-	VisitVariableExpression(expr VariableExpression)
-	VisitKeyValueExpression(expr KeyValueExpression)
-}
-
-type Expression interface {
-	Accept(visitor ExpressionVisitor)
-}
+type Expression interface{}
 
 type AssignExpression struct {
 	Name  tokens.Token
 	Value Expression
-}
-
-func (expr AssignExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitAssignExpression(expr)
 }
 
 type BinaryExpression struct {
@@ -37,37 +17,17 @@ type BinaryExpression struct {
 	Right    Expression
 }
 
-func (expr BinaryExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitBinaryExpression(expr)
-}
-
 type CallExpression struct {
 	Callee    Expression
 	Arguments []Expression
-}
-
-func (expr CallExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitCallExpression(expr)
 }
 
 type GroupingExpression struct {
 	Expression Expression
 }
 
-func (expr GroupingExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitGroupingExpression(expr)
-}
-
-type StringLiteral struct {
-	Value string
-}
-
 type LiteralExpression struct {
 	Value interface{}
-}
-
-func (expr LiteralExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitLiteralExpression(expr)
 }
 
 type LogicalExpression struct {
@@ -76,48 +36,24 @@ type LogicalExpression struct {
 	Right    Expression
 }
 
-func (expr LogicalExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitLogicalExpression(expr)
-}
-
 type UnaryExpression struct {
 	Operator tokens.Token
 	Right    Expression
-}
-
-func (expr UnaryExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitUnaryExpression(expr)
 }
 
 type VariableExpression struct {
 	Name tokens.Token
 }
 
-func (expr VariableExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitVariableExpression(expr)
-}
-
 type ArrayExpression struct {
 	Elements []Expression
-}
-
-func (expr ArrayExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitArrayExpression(expr)
 }
 
 type HashmapExpression struct {
 	Properties []KeyValueExpression
 }
 
-func (expr HashmapExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitHashmapExpression(expr)
-}
-
 type KeyValueExpression struct {
 	Key   tokens.Token
 	Value Expression
-}
-
-func (expr KeyValueExpression) Accept(visitor ExpressionVisitor) {
-	visitor.VisitKeyValueExpression(expr)
 }
--- ast/statement.go
+++ ast/statement.go
@@ -4,38 +4,15 @@ import (
 	"github.com/pmqueiroz/umbra/tokens"
 )
 
-type StatementVisitor interface {
-	VisitBlockStatement(stmt BlockStatement)
-	VisitExpressionStatement(stmt ExpressionStatement)
-	VisitFunctionStatement(stmt FunctionStatement)
-	VisitIfStatement(stmt IfStatement)
-	VisitPrintStatement(stmt PrintStatement)
-	VisitReturnStatement(stmt ReturnStatement)
-	VisitBreakStatement(stmt BreakStatement)
-	VisitVarStatement(stmt VarStatement)
-	VisitPackageStatement(stmt ModuleStatement)
-	VisitInitializedForStatement(stmt InitializedForStatement)
-	VisitConditionalForStatement(stmt ConditionalForStatement)
-}
-
 type Statement interface {
-	Accept(visitor StatementVisitor)
 }
 
 type BlockStatement struct {
 	Statements []Statement
 }
 
-func (stmt BlockStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitBlockStatement(stmt)
-}
-
 type ExpressionStatement struct {
 	Expression Expression
-}
-
-func (stmt ExpressionStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitExpressionStatement(stmt)
 }
 
 type FunctionStatement struct {
@@ -43,27 +20,14 @@ type FunctionStatement struct {
 	Params []tokens.Token
 	Body   []Statement
 }
-
-func (stmt FunctionStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitFunctionStatement(stmt)
-}
-
 type IfStatement struct {
 	Condition  Expression
 	ThenBranch Statement
 	ElseBranch Statement
 }
 
-func (stmt IfStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitIfStatement(stmt)
-}
-
 type PrintStatement struct {
 	Expression Expression
-}
-
-func (stmt PrintStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitPrintStatement(stmt)
 }
 
 type ReturnStatement struct {
@@ -71,25 +35,13 @@ type ReturnStatement struct {
 	Value   Expression
 }
 
-func (stmt ReturnStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitReturnStatement(stmt)
-}
-
 type BreakStatement struct{}
-
-func (stmt BreakStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitBreakStatement(stmt)
-}
 
 type VarStatement struct {
 	Name        tokens.Token
 	Initializer Expression
 	Mutable     bool
 	Type        tokens.Token
-}
-
-func (stmt VarStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitVarStatement(stmt)
 }
 
 type InitializedForStatement struct {
@@ -99,24 +51,12 @@ type InitializedForStatement struct {
 	Body  Statement
 }
 
-func (stmt InitializedForStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitInitializedForStatement(stmt)
-}
-
 type ConditionalForStatement struct {
 	Condition Expression
 	Body      Statement
 }
 
-func (stmt ConditionalForStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitConditionalForStatement(stmt)
-}
-
 type ModuleStatement struct {
 	Name         tokens.Token
 	Declarations []Statement
-}
-
-func (stmt ModuleStatement) Accept(visitor StatementVisitor) {
-	visitor.VisitPackageStatement(stmt)
 }
