97eda74d57e55047b91353f5218f8e0f4d609a31
pmqueiroz <pedroqueiroz593@gmail.com>
2025-09-03 18:41:26 -0300

    feat(native/hashmaps): keys fn

--- native/hashmaps.go
+++ native/hashmaps.go
@@ -9,8 +9,18 @@ func del(args []interface{}) (interface{}, error) {
 	return nil, nil
 }
 
+func keys(args []interface{}) (interface{}, error) {
+	hashmap := args[0].(map[interface{}]interface{})
+	keys := make([]interface{}, 0, len(hashmap))
+	for k := range hashmap {
+		keys = append(keys, k)
+	}
+	return keys, nil
+}
+
 var HashmapModule = InternalModule{
 	symbols: map[string]InternalModuleFn{
 		"delete": del,
+		"keys":   keys,
 	},
 }
